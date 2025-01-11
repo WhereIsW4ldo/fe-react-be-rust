ddb43f2f39f9166df38763643d614d56c17da209
waldo wautelet <waldowautelet4@gmail.com>
2025-01-11 13:56:51 +0100

    feat: parse data from docker ps as struct

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
 use core::fmt;
 use std::process::Command;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 fn main() {
     run_command();
@@ -26,32 +28,59 @@ fn run_command() {
 fn handle_line(line: &str) {
     let components: Vec<&str> = line.split("   ").collect();
 
-    let first = components.first().expect("could not find first in line");
-    let last = components.last().expect("could not find last in line");
-    
-    let container: DockerContainer = DockerContainer::new(last, first);
+    if let Some(container) = DockerContainer::new_from_line(&components)
+    {
+        println!("{container}");
+    }
 
-    println!("{container}");
 }
 
-#[derive(Debug)]
+// CONTAINER ID, IMAGE, COMMAND, CREATED, STATUS, PORTS, NAMES
+
+#[derive(Debug, Serialize)]
 struct DockerContainer {
-    name: String,
-    id: String
+    id: String,
+    image: String,
+    command: String,
+    created: String,
+    status: String,
+    port: String,
+    name: String
 }
 
 impl fmt::Display for DockerContainer {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "Docker with name: {}, id: {}", self.name, self.id)?;
+        writeln!(f, "{}", self.to_string().expect("could not parse to json"))?;
         Ok(())
     }
 }
 
 impl DockerContainer {
-    fn new(name: &str, id: &str) -> DockerContainer {
+    fn new(id: &str, image: &str, command: &str, created: &str, status: &str, port: &str, name: &str) -> DockerContainer {
         return DockerContainer {
-            name: String::from(name),
-            id: String::from(id),
+            id: String::from(id), 
+            image: String::from(image),
+            command: String::from(command), 
+            created: String::from(created),
+            status: String::from(status), 
+            port: String::from(port),
+            name: String::from(name), 
         };
+    }
+
+    fn new_from_line(info: &Vec<&str>) -> Option<DockerContainer> {
+        let (id, rest) = info.split_first().expect("could not split");  
+        let (image, rest) = rest.split_first().expect("could not split");  
+        let (command, rest) = rest.split_first().expect("could not split");  
+        let (created, rest) = rest.split_first().expect("could not split");  
+        let (status, rest) = rest.split_first().expect("could not split");  
+        let (port, rest) = rest.split_first().expect("could not split");  
+        let name = rest.first().expect("could not take first");
+        
+        Some(DockerContainer::new(id, image, command, created, status, port, name))
+    }
+
+    fn to_string(&self) -> Result<String> {
+        serde_json::to_string(&self)
     }
 }
