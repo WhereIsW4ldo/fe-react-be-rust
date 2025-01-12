35146777271f3068013a04bb939524cf6430f3e9
waldo wautelet <waldowautelet4@gmail.com>
2025-01-12 13:03:04 +0100

    feat: frontend updates every 1 second

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,30 +1,35 @@
-import {FlatList, Pressable, StyleSheet, Text, View} from "react-native";
+import {Pressable, ScrollView, StyleSheet, Text, View} from "react-native";
 import {useEffect, useState} from "react";
+import { Table, Row, Rows } from 'react-native-table-component';
 
 export default function Index() {
     const [dockers, setDockers] = useState<DockerContainer[]>([]);
     const [dataLoading, setDataLoading] = useState<boolean>(false);
+    
+    const headers = ['name', 'id', 'image', 'command', 'created', 'status'];
+
+    const getData = () => {
+        fetch('http://127.0.0.1:8000/sudo/docker/ps')
+            .then(response => response.text())
+            .then(json => {
+                console.log(json);
+                const jsonValue: DockerContainer[] = JSON.parse(json);
+
+                return jsonValue;
+            })
+            .then(dockers => {
+                setDockers(dockers);
+                console.log(dockers);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     useEffect(() => {
-        const getData = () => {
-            fetch('http://127.0.0.1:8000/sudo/docker/ps')
-                .then(response => response.text())
-                .then(json => {
-                    console.log(json);
-                    const jsonValue: DockerContainer[] = JSON.parse(json);
-
-                    return jsonValue;
-                })
-                .then(dockers => {
-                    setDockers(dockers);
-                    console.log(dockers);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        }
-        getData();
-    }, [dataLoading])
+        const timer = setInterval(() => getData(), 1000);
+        return () => clearInterval(timer);
+    }, [])
     
     return (
     <View
@@ -34,65 +39,48 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <FlatList 
-            data={dockers} 
-            keyExtractor={({id}) => id} 
-            renderItem={({item}) => (
-              <View style={styles.listView}>
-                  <Text style={styles.text}>name: {item.name}</Text>
-                  <Text style={styles.text}>id: {item.id}</Text>
-                  <Text style={styles.text}>image: {item.image}</Text>
-                  <Text style={styles.text}>command: {item.command}</Text>
-                  <Text style={styles.text}>created: {item.created}</Text>
-                  <Text style={styles.text}>status: {item.status}</Text>
-              </View>
-            )}
-        />
-        <Pressable onPress={() => {setDataLoading((loading) => !loading)}}>
-            <Text style={styles.text}>Refetch!</Text>
-        </Pressable>
+        <ScrollView style={styles.container}>
+            <Table borderStyle={{borderWidth: 2}}>
+                <Row data={headers} style={styles.head} textStyle={styles.text}/>
+                <Rows data={dockers.map((x) => {return [x.Names, x.ID, x.Image, x.Command, x.CreatedAt, x.Status];})} textStyle={styles.text}/>
+            </Table>
+        </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    listView: {
-        flex: 1,
-        justifyContent: "space-between",
-        alignItems: "center",
-    }
+    container: { padding: 16, paddingTop: 30, width: 1000},
+    head: { height: 40, backgroundColor: '#f1f8ff' },
+    text: { margin: 6 }
 });
 
 type IDockerContainer = {
-    id: string,
-    image: string,
-    command: string,
-    created: string,
-    status: string,
-    ports: string[],
-    name: string
+    ID: string,
+    Image: string,
+    Command: string,
+    CreatedAt: string,
+    Status: string,
+    Ports: string,
+    Names: string
 }
 
 class DockerContainer implements IDockerContainer {
-    id: string;
-    image: string;
-    command: string;
-    created: string;
-    status: string;
-    ports: string[];
-    name: string;
+    ID: string;
+    Image: string;
+    Command: string;
+    CreatedAt: string;
+    Status: string;
+    Ports: string;
+    Names: string;
     
-    constructor(id: string, image: string, command: string, created: string, status: string, ports: string[], name: string) {
-        this.id = id;
-        this.image = image;
-        this.command = command;
-        this.created = created;
-        this.status = status;
-        this.ports = ports;
-        this.name = name;
+    constructor(id: string, image: string, command: string, created: string, status: string, ports: string, name: string) {
+        this.ID = id;
+        this.Image = image;
+        this.Command = command;
+        this.CreatedAt = created;
+        this.Status = status;
+        this.Ports = ports;
+        this.Names = name;
     }
 }
