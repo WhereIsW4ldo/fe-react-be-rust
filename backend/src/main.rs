966fd5c7961c442749d63b3652688d0bc309a29f
waldo wautelet <waldowautelet4@gmail.com>
2025-01-12 13:02:51 +0100

    feat: change backend for JSON of docker command

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,8 @@
-use std::fmt;
-use std::process::Command;
+use std::{fmt, io};
+use std::io::Write;
+use std::process::{Command, Stdio};
 use rocket_cors::{AllowedMethods};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[macro_use]
 extern crate rocket;
@@ -22,43 +23,34 @@ fn rocket() -> _ {
 
 #[get("/sudo/docker/ps")]
 fn run_command() -> String {
-    let output = Command::new("sudo")
-        .arg("docker")
-        .arg("ps")
+    let argument = "sudo docker ps --all --no-trunc --format=\"{{json . }}\" | jq --tab -s .";
+
+    let sudo_docker = Command::new("sh")
+        .arg("-c")
+        .arg(argument)
+        .stderr(Stdio::piped())
         .output()
         .expect("failed to execute hello");
 
-    let string = String::from_utf8(output.stdout).expect("failed to stringify output");
+    // let string = String::from_utf8(output.stdout).expect("failed to stringify output");
 
-    let split_string = string.lines().skip(1);
+    println!("{}", sudo_docker.status);
+    let string = String::from_utf8(sudo_docker.stdout).expect("failed to stringify");
+    let dockers: Vec<DockerContainer> = serde_json::from_str(&string).unwrap();
 
-    let lines = split_string
-        .map(|line| handle_line(line))
-        .flatten()
-        .collect::<Vec<_>>();
-
-    if let Ok(data) = serde_json::to_string(&lines)
-    {
-        return data;
-    }
-
-    "".to_string()
+    serde_json::to_string(&dockers).unwrap()
 }
 
-fn handle_line(line: &str) -> Option<DockerContainer>{
-    let components: Vec<&str> = line.split("   ").collect();
-    DockerContainer::new_from_line(&components)
-}
-
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct DockerContainer {
-    id: String,
-    image: String,
-    command: String,
-    created: String,
-    status: String,
-    ports: Vec<String>,
-    name: String
+    ID: String,
+    Image: String,
+    Command: String,
+    CreatedAt: String,
+    Status: String,
+    Ports: String,
+    Names: String,
+    State: String,
 }
 
 impl fmt::Display for DockerContainer {
@@ -69,30 +61,6 @@ impl fmt::Display for DockerContainer {
 }
 
 impl DockerContainer {
-    fn new(id: &str, image: &str, command: &str, created: &str, status: &str, ports: &str, name: &str) -> DockerContainer {
-        DockerContainer {
-            id: String::from(id),
-            image: String::from(image),
-            command: String::from(command),
-            created: String::from(created),
-            status: String::from(status),
-            ports: ports.split(", ").map(String::from).collect(),
-            name: String::from(name),
-        }
-    }
-
-    pub fn new_from_line(info: &Vec<&str>) -> Option<DockerContainer> {
-        let (id, rest) = info.split_first().expect("could not split");
-        let (image, rest) = rest.split_first().expect("could not split");
-        let (command, rest) = rest.split_first().expect("could not split");
-        let (created, rest) = rest.split_first().expect("could not split");
-        let (status, rest) = rest.split_first().expect("could not split");
-        let (ports, rest) = rest.split_first().expect("could not split");
-        let name = rest.first().expect("could not take first");
-
-        Some(DockerContainer::new(id, image, command, created, status, ports, name))
-    }
-
     fn to_string(&self) -> Option<String> {
         serde_json::to_string(&self).ok()
     }
